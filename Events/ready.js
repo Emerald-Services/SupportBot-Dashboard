51ffafeeda3c48c09173030f7d25aa6a0c8a6f5a
1tzEmerald <xiledemerald@gmail.com>
2026-07-26 14:23:55 +0100

    Dashboard Int.

--- Events/ready.js
+++
@@ -1,8 +0,0 @@
-const Event = require("../Structures/Event.js");
-const { runBotReady } = require("../Structures/BotStartup.js");
-
-module.exports = new Event("clientReady", async (client) => {
-  // restartDiscordBot runs startup manually — clientReady often does not re-fire anyway
-  if (client.__explicitStartup) return;
-  await runBotReady(client, { clearConsole: true, logCommands: false });
-});
--- Structures/BotInvite.js
+++
@@ -1,6 +0,0 @@
-function getBotInviteUrl(clientId) {
-  if (!clientId) return null;
-  return `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot%20applications.commands`;
-}
-
-module.exports = { getBotInviteUrl };
